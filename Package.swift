a8a5d9bde8066963a1d71a007adbc782a1a30182
endpne <ourfor@qq.com>
2026-06-18 12:47:51 +0800

    ♻️ refactor code

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "V8",
-            url: "https://github.com/kintcp/ScriptKit/releases/download/spm-v1.0.9/V8.xcframework.zip",
-            checksum: "6713ae26e3cc81445c71400b214af6a315bb967774c11a5c541c59ef8ed7d404"),
+            url: "https://github.com/kintcp/ScriptKit/releases/download/spm-v1.0.10/V8.xcframework.zip",
+            checksum: "08694dbdb21e42ecabaca205d732a996416f84099fd26839d25da5e1ad79ea4c"),
         .binaryTarget(
             name: "QuickJS",
             url: "https://github.com/kintcp/ScriptKit/releases/download/quickjs-spm-v1.0.10/QuickJS.xcframework.zip",
